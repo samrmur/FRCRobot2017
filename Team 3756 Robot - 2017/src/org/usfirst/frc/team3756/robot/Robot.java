a836316ee0d0c87a7a21944d2fa0a897140485fd
Omar Lopez <omar.lopez.cae@gmail.com>
2017-02-03 16:50:18 -0500

    Update Robot.java

--- Team 3756 Robot - 2017/src/org/usfirst/frc/team3756/robot/Robot.java
+++ Team 3756 Robot - 2017/src/org/usfirst/frc/team3756/robot/Robot.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team3756.robot;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.NamedSendable;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team3756.robot.commandgroups.AutoCommandGroup1;
 import org.usfirst.frc.team3756.robot.commandgroups.AutoCommandGroup2;
+import org.usfirst.frc.team3756.robot.commandgroups.TestEncoderCommand;
 import org.usfirst.frc.team3756.robot.subsystems.DriveEncoder;
 import org.usfirst.frc.team3756.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team3756.robot.subsystems.LimitSwitch;
@@ -48,29 +49,29 @@ public class Robot extends IterativeRobot {
 		// Initialize all subsystems
         driveTrain = new DriveTrain();
 		oi = new OI();
-		limitSwitch = new LimitSwitch();
+		limitSwitch = new LimitSwitch();                                                                                                                                                                                                                                                               
 		encoder = new DriveEncoder();
-		
-		// Configure camera 
-		lifeCam = CameraServer.getInstance().startAutomaticCapture();
-		lifeCam.setResolution(320, 240);
-		lifeCam.setFPS(15);
+
+		// Configure camera                                                                                 
+//		lifeCam.setResolution(320, 240);
+//		lifeCam.setFPS(15);
 		
 		// Initialize chooser, add commands to chooser and display on the SmartDashboard
 		autoChooser = new SendableChooser<>();
 		autoChooser.addDefault("Default Command", new AutoCommandGroup1());
 		autoChooser.addObject("Position 1 Command", new AutoCommandGroup2());
+		autoChooser.addObject("Test Encoder", new TestEncoderCommand());
 		
 		// Show what command your subsystem is running on the SmartDashboard
-		SmartDashboard.putString("autoName", "Autonomous Commands");
 		SmartDashboard.putData("autoChooser", autoChooser);
         SmartDashboard.putData(driveTrain);
-        SmartDashboard.putData(limitSwitch);
+        SmartDashboard.putData(limitSwitch); 
+    
 	} // End of method
-
+	
 	/*
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
+	 * between different autonomous modes using the dashboard. The sendable\\
 	 * chooser code works with the Java SmartDashboard. If you prefer the
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
 	 * getString code to get the auto name from the text box below the Gyro
@@ -104,7 +105,15 @@ public class Robot extends IterativeRobot {
 	// This function is called periodically during operator control
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+	  Scheduler.getInstance().run();
+	  // Show clicks of encoder everytime the wheel turns
+      SmartDashboard.putNumber("Current Count of Right Wheel Encoder", Robot.encoder.showRightClicks());
+      SmartDashboard.putNumber("Current Count of Left Wheel Encoder", Robot.encoder.showLeftClicks()); 
+      SmartDashboard.putNumber("Current Count of Right Wheel Distance", Robot.encoder.getRightWheelDistance()); 
+      SmartDashboard.putNumber("Current Count of Left Wheel Distance", Robot.encoder.getLeftWheelDistance());
+      SmartDashboard.putNumber("Current Count of Right Wheel Rate", Robot.encoder.getRightRate()); 
+      SmartDashboard.putNumber("Current Count of Left Wheel Rate", Robot.encoder.getLeftRate());
+      
 	} // End of method
 
 	// This function is called periodically during test mode
