dfc92aad2d44441240c8dba68a6cd1e16dff1738
Omar Lopez <omar.lopez.cae@gmail.com>
2017-02-03 16:48:59 -0500

    Update DriveEncoder.java

--- Team 3756 Robot - 2017/src/org/usfirst/frc/team3756/robot/subsystems/DriveEncoder.java
+++ Team 3756 Robot - 2017/src/org/usfirst/frc/team3756/robot/subsystems/DriveEncoder.java
@@ -9,11 +9,13 @@ package org.usfirst.frc.team3756.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.tables.ITable;
 
 // Start of DriveEncoder
 public class DriveEncoder extends Subsystem {
 	// Declare Encoder object
-	public Encoder wheelEncoder;
+	private Encoder rightWheelEncoder;
+	private Encoder leftWheelEncoder;
 	
 	/* Declare and initialize variables 
 	 * These variables are measured by the team and not by the program 
@@ -21,8 +23,8 @@ public class DriveEncoder extends Subsystem {
 	 * a fixed distance and enter the distance you pushed it below 
 	 * and the number of counts your encoder reported for the distance
 	 */
-	private static final double DISTANCE_MEASURED = 0; // Change as needed
-	private static final double PULSE_MEASURED = 0; // Change as needed
+	private static final double DISTANCE_MEASURED = 3; // Change as needed
+	private static final double PULSE_MEASURED = 2; // Change as needed
 	
 	// Get the distance per pulse
 	private static final double DISTANCE_PER_PULSE = DISTANCE_MEASURED / PULSE_MEASURED;
@@ -32,27 +34,68 @@ public class DriveEncoder extends Subsystem {
 	 */
 	public DriveEncoder() {
 		// Instantiate Encoder object
-		wheelEncoder = new Encoder(8, 9, false, Encoder.EncodingType.k4X);
-		
+		// Connection is at 4 instead of 1 because there seems t be a problem with port 1
+		rightWheelEncoder = new Encoder(0, 4, true, Encoder.EncodingType.k4X);
+		leftWheelEncoder = new Encoder(2, 3, true, Encoder.EncodingType.k4X);
+	
 		// Setup Encoder object
-		wheelEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+		rightWheelEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+		leftWheelEncoder.setDistancePerPulse(DISTANCE_PER_PULSE); 
+	
+//		rightWheelEncoder.setMaxPeriod(0.1);
+//		rightWheelEncoder.setMinRate(10);
+//		rightWheelEncoder.setReverseDirection(true);
+//		rightWheelEncoder.setSamplesToAverage(7);
+//		
+//		leftWheelEncoder.setMaxPeriod(0.1);
+//		leftWheelEncoder.setMinRate(10);
+//		leftWheelEncoder.setReverseDirection(true);
+//		leftWheelEncoder.setSamplesToAverage(7);
 	} // End of constructor 
+	
+	public int showRightClicks(){
+		return rightWheelEncoder.getRaw();
+	}
+	
+	public int showLeftClicks(){
+		return leftWheelEncoder.getRaw();
+	}
 	
 	/**
 	 *  Reset the count of the encoder
 	 */
 	public void reset() {
-		wheelEncoder.reset();
+		rightWheelEncoder.reset();
+		leftWheelEncoder.reset();
 	} // End of method
 	
 	/**
 	 * Get the current distance the robot has traveled
 	 */
-	public double getDistance() {
-		return wheelEncoder.getDistance();
+	public double getRightWheelDistance() {
+		return rightWheelEncoder.getDistance();
 	} // End of method
 	
-    public void initDefaultCommand() {
+	public double getLeftWheelDistance() {
+		return leftWheelEncoder.getDistance();
+	} // End of method
+	
+	public ITable getRightTable(){
+		return rightWheelEncoder.getTable();
+	}
+	
+	public ITable getLeftTable(){
+		return leftWheelEncoder.getTable();
+	}
+	
+	public double getRightRate(){
+		return rightWheelEncoder.getRate();
+	}
+	
+	public double getLeftRate(){
+		return leftWheelEncoder.getRate();
+	}
+	
+	public void initDefaultCommand() {
     } // End of method
 } // End of class
-
